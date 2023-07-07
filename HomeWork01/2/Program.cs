2be9f95a2a99024e88c110b64b88989f1acd7223
NataK <natak9423@gmail.com>
2023-07-07 04:14:55 +0300

    second  exercise

---
+++ HomeWork01/2/Program.cs
@@ -0,0 +1,26 @@
+﻿int num1;
+int num2;
+int num3;
+num1 = new Random().Next(1,10);
+num2 = new Random().Next(1,10);
+num3 = new Random().Next(1,10);
+Console.WriteLine($"Из чисел: {num1}, {num2}, {num3}");
+
+//Console.Write("Введите первое число: ");
+//num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//num2 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Максимальное число: ");
+if (num1 > num2) 
+    if (num1 > num3)
+        Console.WriteLine(num1);
+    else 
+        Console.WriteLine(num3);
+else 
+    if (num2 > num3)
+        Console.WriteLine(num2);
+    else 
+        Console.WriteLine(num3);
