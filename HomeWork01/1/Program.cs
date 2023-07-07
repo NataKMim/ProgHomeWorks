4834d4f0f1b5d54a3d57702f9c24051129d0a50f
NataK <natak9423@gmail.com>
2023-07-07 04:00:39 +0300

    first exercise

---
+++ HomeWork01/1/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1;
+int num2;
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное число: ");
+if (num1 > num2) Console.WriteLine(num1);
+else Console.WriteLine(num2);
