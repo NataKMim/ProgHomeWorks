ab0a62b57d1abf3991b69092bc090a2984fa62ab
NataK <natak9423@gmail.com>
2023-07-07 04:22:52 +0300

    third exercise

---
+++ HomeWork01/3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+
+//num = new Random().Next(1,10);
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число {num} ");
+
+if (num%2 == 0) 
+    Console.WriteLine("четное");
+else 
+    Console.WriteLine("нечетное");
