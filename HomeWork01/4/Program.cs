dea30c1f99d501fea7bd62bf8229e92c20de5c18
NataK <natak9423@gmail.com>
2023-07-07 04:36:45 +0300

    fourth exercise

---
+++ HomeWork01/4/Program.cs
@@ -0,0 +1,16 @@
+﻿int num;
+int i = 2;
+
+//num = new Random().Next(1,10);
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Четные числа до {num}: ");
+if (i > num) 
+    Console.Write(" - ");
+else
+    while (i <= num)
+    {
+        Console.Write($"{i} ");
+        i+=2;
+    }
